58cc05c9b7346454a62749697dae4fb6bac85d0d
Andrei Alekseev <Andrei Alekseev>
2016-10-17 22:58:07 +0300

    from home

--- gulpfile.js
+++ gulpfile.js
@@ -15,10 +15,13 @@ gulp.task('compass', function() {
 });
 
 gulp.task('concat', function(){
-	return gulp.src(['./js/src/jquery.min.js', './js/src/jquery.fancybox.pack.js', './js/src/my.js'])
+	return gulp.src(['./js/src/jquery.min.js', './js/src/jquery.fancybox.pack.js', './js/src/slick.min.js', './js/src/my.js'])
 	.pipe(concat('./js/project.js'))
 	.pipe(gulp.dest('./'))
 });
+
+gulp.watch('./sass/*.scss', ['compass']);
+gulp.watch('./js/src/my.js', ['concat']);
 
 gulp.task('default', ['concat', 'compass'], function() {
 	browserSync.init({
